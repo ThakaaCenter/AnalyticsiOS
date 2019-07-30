433c2fef3bea909f1a88e5ad67c18c4670b023b5
nkabour <nadak.k@thakaa.sa>
2019-07-30 18:53:41 +0300

    Initial Commit

---
+++ iOSDemo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  iOSDemo
+//
+//  Created by nada kk on 30/07/2019.
+//  Copyright © 2019 nada kk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOSDemo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iOSDemo
+//
+//  Created by nada kk on 30/07/2019.
+//  Copyright © 2019 nada kk. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
